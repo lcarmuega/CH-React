ef2a6bb0deb4bf101485a71bad7ffae0523be9cd
Lucia Carmuega <lu.carmuega@gmail.com>
2022-04-20 16:36:46 -0300

    Desafio 10

--- src/components/ItemCard/index.jsx
+++ src/components/ItemCard/index.jsx
@@ -7,7 +7,7 @@ const ItemCard = ({id, title, price, pictureUrl, stock}) => {
     return (
         <div className='productContainer' id={id}>
             <h6 className='productTitle'><Link to={`/item/${id}`}>{title}</Link></h6>
-            <img src={require("../../assets/book-example.jpg")} alt="Tapa del libro" className='productImage'/>
+            <img src={pictureUrl} alt="Tapa del libro" className='productImage'/>
             <span className='productPrice'>${price}</span>
             <ItemCount valorStock={stock} />
         </div>
--- src/components/ItemListContainer/index.jsx
+++ src/components/ItemListContainer/index.jsx
@@ -2,32 +2,27 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import ItemList from '../ItemList';
 import './styles.css';
+import { db } from '../../firebase/config';
+import { collection, getDocs } from "firebase/firestore";
 
 const ItemListContainer = () => {
   const [products, setProducts] = useState([]);
-  const {id} = useParams();
+  const { id } = useParams();
 
   useEffect(() => {
     (async () => {
-
-      const promesa = new Promise((acc, rej) => {
-        const response = fetch('/data.json');
-        setTimeout(() => {
-          acc(response);
-        }, 1000)
-      })
-
       try {
-        const respuesta = await promesa;
-        const data = await respuesta.json();
-        const products = data.products;
+        const querySnapshot = await getDocs(collection(db, "productos"));
+        const dbProducts = []
+        querySnapshot.forEach((doc) => {
+          dbProducts.push({ id: doc.id, ...doc.data() })
+        });
         if (!id) {
-          setProducts(products);
+          setProducts(dbProducts);
         } else {
-          const filteredProduts = products.filter(prod => prod.category === id);
-          setProducts(filteredProduts);
+          const filteredProducts = dbProducts.filter(prod => prod.category === id)
+          setProducts(filteredProducts);
         }
-
       } catch (error) {
         console.log(error);
       }
